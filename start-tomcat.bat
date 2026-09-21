5c29f3980b0947fbfeedcdfe7d57a07d993a5461
Jeevanbnj <jbn83757@gmail.com>
2026-09-21 11:10:13 +0530

    kjewfblwjebf

---
+++ start-tomcat.bat
@@ -0,0 +1,4 @@
+@echo off
+set JAVA_HOME=C:\Program Files\Java\jdk-17
+set CATALINA_HOME=C:\Users\Jeevan bn\Downloads\apache-tomcat-9.0.121-windows-x64\apache-tomcat-9.0.121
+call "%CATALINA_HOME%\bin\catalina.bat" run
